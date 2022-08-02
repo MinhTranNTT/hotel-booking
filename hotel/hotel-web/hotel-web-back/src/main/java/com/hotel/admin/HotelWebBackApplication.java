5a75a8fadb8838a70a3e4e5495369a93099dbabe
MinhTranNTT <tvhuuminhntt@gmail.com>
2022-08-02 23:19:48 +0700

    init project

---
+++ hotel/hotel-web/hotel-web-back/src/main/java/com/hotel/admin/HotelWebBackApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelWebBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelWebBackApplication.class, args);
+	}
+
+}
---
+++ hotel/hotel-web/hotel-web-front/src/main/java/com/hotel/site/HotelWebFrontApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelWebFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelWebFrontApplication.class, args);
+	}
+
+}
